5c28ad2f22d67e044b8513b7e5a58a536ed1825d
Martin Marchev <martin@dreambox.biz>
2013-03-20 22:05:23 +0200

    Second servlet modifications

--- HelloJavaWebWorld/src/main/java/biz/dreambox/tutorials/servlets/SecondServlet.java
+++ HelloJavaWebWorld/src/main/java/biz/dreambox/tutorials/servlets/SecondServlet.java
@@ -1,6 +1,8 @@
 package biz.dreambox.tutorials.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.MessageFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,10 +16,23 @@ public class SecondServlet extends HttpServlet
     private static final long serialVersionUID = -1757531409531136302L;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException
     {
-        resp.getOutputStream().println("Second servlet!");
+        System.out.println("click");
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        response.setContentType("text/html");
+        
+        PrintWriter out = response.getWriter();
+        String userName = request.getParameter("userName");
+        String pageOutput = MessageFormat.format("<html><head><title>Here we go!</title></head><body>Hello, {0}!</body></html>",
+                                                 new Object[] { userName  });
+        out.println(pageOutput);
     }
 
 }
